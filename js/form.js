21d13ea78c85df74284f7628aefc12c3ef172989
Bast Paq <bastpaq@MacBook-Pro-de-Bast.local>
2019-11-23 18:58:15 +0100

    add form

--- js/form.js
+++ js/form.js
@@ -1,14 +1,27 @@
 let inputs = document.querySelectorAll(".blur-verif");
 let mail = document.querySelector(".key-verif");
+let form = document.querySelector("form");
+let overlay = document.querySelector(".form-overlay");
+let alertOk = document.querySelector(".alert-ok");
+let alertWrong = document.querySelector(".alert-wrong");
+let result = false;
 
 let isValid = function(element, condition) {
+  var parent = element.parentNode;
   if (condition) {
-    element.classList.remove("is-not-valid");
-    element.classList.add("is-valid");
+    parent.classList.remove("is-not-valid");
+    parent.classList.add("is-valid");
+    result = true;
   } else {
-    element.classList.remove("is-valid");
-    element.classList.add("is-not-valid");
+    parent.classList.remove("is-valid");
+    parent.classList.add("is-not-valid");
+    result = false;
   }
+};
+
+let resetForm = function(element) {
+  element.value = "";
+  element.parentNode.classList.remove("is-valid");
 };
 
 inputs.forEach(function(elt) {
@@ -17,6 +30,42 @@ inputs.forEach(function(elt) {
   });
 });
 
-mail.addEventListener("keyup", function() {
+mail.addEventListener("blur", function() {
   isValid(mail, /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/i.test(mail.value));
+});
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  if (result) {
+    overlay.style.display = "flex";
+    alertOk.style.display = "block";
+
+    setTimeout(() => {
+      overlay.style.display = "none";
+      alertOk.style.display = "none";
+    }, 3000);
+
+    for (let i of inputs) {
+      resetForm(i);
+    }
+
+    resetForm(mail);
+
+    document.querySelector("textarea").value = "";
+  } else {
+    overlay.style.display = "flex";
+    alertWrong.style.display = "block";
+
+    inputs.forEach(function(elt) {
+      isValid(elt, elt.value);
+    });
+
+    isValid(mail, /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/i.test(mail.value));
+
+    setTimeout(() => {
+      overlay.style.display = "none";
+      alertWrong.style.display = "none";
+    }, 3000);
+  }
 });
